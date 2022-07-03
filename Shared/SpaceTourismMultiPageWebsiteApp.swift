d23fccce28cd356910cb2bbeaa4c967a4e049a4b
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-07-03 10:09:18 +0900

    Initial Commit

---
+++ Shared/SpaceTourismMultiPageWebsiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceTourismMultiPageWebsiteApp.swift
+//  Shared
+//
+//  Created by Jinwook Kim on 2022/07/03.
+//
+
+import SwiftUI
+
+@main
+struct SpaceTourismMultiPageWebsiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
