a5dcbd0ae754906c78163df2f086aa2d62f11add
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-07-05 16:03:16 +0900

    Add the destination header view.

---
+++ Shared/Views/DestinationHeaderView.swift
@@ -0,0 +1,29 @@
+//
+//  DestinationHeaderView.swift
+//  Space Tourism Multi Page Website (iOS)
+//
+//  Created by Jinwook Kim on 2022/07/05.
+//
+
+import SwiftUI
+
+struct DestinationHeaderView: View {
+    var body: some View {
+        HStack(spacing: 16) {
+            Text("01")
+                .fontWeight(.semibold)
+                .foregroundColor(Color("TitleColor"))
+            Text("PICK YOUR DESTINATION")
+                .fontWeight(.semibold)
+                .foregroundColor(.white)
+        }
+    }
+}
+
+struct DestinationHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        DestinationHeaderView()
+            .background(Color("BackgroundColor"))
+            .previewLayout(.sizeThatFits)
+    }
+}
--- Shared/Views/DestinationView.swift
+++ Shared/Views/DestinationView.swift
@@ -9,12 +9,16 @@ import SwiftUI
 
 struct DestinationView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            DestinationHeaderView()
+        }
     }
 }
 
 struct DestinationView_Previews: PreviewProvider {
     static var previews: some View {
         DestinationView()
+            .background(Color("BackgroundColor"))
+            .previewLayout(.sizeThatFits)
     }
 }
